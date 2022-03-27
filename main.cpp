3f8ceae92f0aa4288eba5eac1f0e04c4e798838f
witch_m <good99margaret@gmail.com>
2022-03-27 21:54:47 +0300

    task 1.7

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
+
+#define TIME_TEST(testCode, message) { \
+clock_t start_time = clock(); \
+testCode \
+clock_t end_time = clock();\
+clock_t code_time = end_time - start_time; \
+std::clog << message << ": " \
+<< (double) code_time / CLOCKS_PER_SEC << std::endl; \
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::setvbuf(stdout, nullptr, _IOFBF, 1000000);
+
+    const char *filename = "tmp.txt";
+    std::ofstream file(filename);
+
+    TIME_TEST ({
+                   for (int i = 0; i < 1000000; i++)
+                       file << 'a' << std::endl;
+               }, " Often buffer reset ");
+
+    TIME_TEST ({
+                   for (int i = 0; i < 1000000; i++)
+                       file << 'a' << '\n';
+               }, " Buffer opt");
+
+    file.close();
+    std::remove(filename);
+
     return 0;
 }
